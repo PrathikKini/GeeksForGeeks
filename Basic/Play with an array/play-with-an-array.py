55db46579e32b071645554ae678243ee28bfb588
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2023-10-13 12:10:21 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Play with an array/play-with-an-array.py
@@ -0,0 +1,36 @@
+def formatArray(a,n):
+    # add code here
+    for i in range(len(a)):
+        if i%2 != 0:
+            if a[i] < a[i-1]:
+                temp = a[i]
+                a[i] = a[i-1]
+                a[i-1] = temp
+    return a
+
+#{ 
+ # Driver Code Starts
+if __name__=="__main__":
+    t=int(input())
+    for j in range(t):
+        n=int(input())
+        a=list(map(int,input().split()))
+        b=formatArray(a,n)
+        flag= 1
+        if(len(b)==len(a)):
+            for k in range(1,n,2):
+                if(b[k]<b[k-1]):
+                    flag=0
+            if(flag==0):
+                print(0)
+            else:
+                b.sort()
+                a.sort()
+                for p in range(0,n):
+                    if(a[p]!=b[p]):
+                        flag=0
+                print(flag)
+        else:
+            print(0)
+
+# } Driver Code Ends
