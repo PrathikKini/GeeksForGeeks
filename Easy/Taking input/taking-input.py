55629a86520e1d911c408c612dac7fd6f8877f2b
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2024-05-14 19:58:22 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Taking input/taking-input.py
@@ -0,0 +1,19 @@
+
+class Solution:
+    def inputData(self) -> None:
+        # code here
+        a = int(input())
+        b = input()
+        print(a,b) 
+
+
+
+#{ 
+ # Driver Code Starts
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        obj = Solution()
+        obj.inputData()
+
+# } Driver Code Ends
