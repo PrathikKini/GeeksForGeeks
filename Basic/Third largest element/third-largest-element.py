3e4213a1aecd4deb7ac54ae77707ee4b963ae147
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2024-05-27 01:39:58 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Third largest element/third-largest-element.py
@@ -0,0 +1,28 @@
+class Solution:
+    def thirdLargest(self,a, n):
+        # code here
+        first = second = third = float('-inf')
+        
+        for num in a:
+            if num > first:
+                third = second
+                second = first
+                first = num
+            elif num > second:
+                third = second
+                second = num
+            elif num > third:
+                third = num
+        return third
+                
+
+#{ 
+ # Driver Code Starts
+# Your code goes here
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        print(Solution().thirdLargest(arr, n))
+# } Driver Code Ends
