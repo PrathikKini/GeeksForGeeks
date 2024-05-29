6ebe4bc3f258a4d8ca0dd44d72d54502edefc4e4
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2024-05-30 01:07:35 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Repeated sum of digits/repeated-sum-of-digits.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+class Solution:
+    def repeatedSumOfDigits (self,N):
+        # code here 
+        while N >= 10:
+            N = sum(int(i) for i in str(N))
+        return N
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        
+        N=int(input())
+      
+        ob = Solution()
+        print(ob.repeatedSumOfDigits(N))
+# } Driver Code Ends
