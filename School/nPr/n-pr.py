cc4c912ec96140f1b4372f250052a4f51faa09bc
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2024-05-31 00:10:24 +0530

    Added Solution - GfG to GitHub

---
+++ School/nPr/n-pr.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+
+class Solution:
+    def nPr(self, n, r):
+        # code here
+        f = 1
+        for i in range(1,r+1):
+            f *= (n-i+1)
+        return f
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n, r = [int(x) for x in input().split()]
+        
+        ob = Solution()
+        print(ob.nPr(n, r))
+# } Driver Code Ends
