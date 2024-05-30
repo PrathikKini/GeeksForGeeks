369971347c122bfb4f515b6e3743eb244954ab22
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2024-05-30 23:57:38 +0530

    Added Solution - GfG to GitHub

---
+++ School/Sum of GP/sum-of-gp.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+
+class Solution:
+	def sum_of_gp(self, n, a, r):
+		# Code here
+		if r == 1:
+		    return n*a
+		return int(a*(1-r**n) / (1-r))
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n, a, r = input().split()
+		n = int(n)
+		a = int(a)
+		r = int(r)
+		ob = Solution();
+		ans = ob.sum_of_gp(n, a, r)
+		print(ans)
+# } Driver Code Ends
