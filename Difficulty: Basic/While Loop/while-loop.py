c985c3fdffa0103a1fc5505e1f3759f911ee87d9
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2025-05-23 11:15:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/While Loop/while-loop.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+
+
+# Function to print x in decreasing order
+def utility(x):
+    # Complete the code below
+    i = x
+    while i>=0:
+        print(i,end = " ")
+        i-=1
