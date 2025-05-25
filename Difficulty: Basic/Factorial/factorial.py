a07673901013ae647429a35d5bffef96487c8d12
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2025-05-25 16:00:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.py
+++ Difficulty: Basic/Factorial/factorial.py
@@ -1,12 +1,8 @@
 #User function Template for python3
+n = int(input())
 
-
-def nFactorial(n):
-
-    ans = 1
-
-    #Write your code below
-    for i in range(2,n+1):
-        ans *= i
-
-    return ans
+# Your code here
+result = 1
+for i in range(2,n+1):
+    result *= i
+print(result)
