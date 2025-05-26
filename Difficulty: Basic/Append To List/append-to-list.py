30f072dd0b038f4e486f5f4c9d8eb6aab27fa945
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2025-05-26 18:05:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Append To List/append-to-list.py
@@ -0,0 +1,13 @@
+#User function Template for python3
+
+def appendToList(a,b,c):
+    #Write the code below
+    #Create a list
+    l = []
+    #Append a b c in order
+    l.append(a)
+    l.append(b)
+    l.append(c)
+    #Return the list
+    return l
+    #Dont print here
