edceff9f07c2c8c7a3d7a790b6b17972c48915a2
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2025-05-26 16:44:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Fibonacci Number/fibonacci-number.py
+++ Difficulty: Basic/Fibonacci Number/fibonacci-number.py
@@ -1,8 +1,15 @@
 #User function Template for python3
-def fibonacci(n):
-    #your code here
-    # return nth fibonacci element
-    a, b = 0, 1
-    for _ in range(2,n+1):
-        a, b = b, a+b
-    return b
+#Back-end complete function Template for Python 3
+n = int(input())
+
+########### Write your code below ###############
+a = 0
+b = 1
+l = [0,1]
+for i in range(2,n+1):
+    c = a + b
+    a = b
+    b = c
+    l.append(b)
+########### Write your code above ###############
+print(l[-1])
