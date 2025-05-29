cf36ff97f8f076267faf14fc1748dfae030ee471
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2025-05-30 01:45:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Compare two Large Numbers/compare-two-large-numbers.py
@@ -0,0 +1,12 @@
+#User function Template for python3
+
+class Solution:
+
+    def check(self, a,b):
+        # code here
+        if a == b:
+            return 3
+        elif a > b:
+            return 2
+        else:
+            return 1
