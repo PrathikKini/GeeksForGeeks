2e74d83c77cdd06fee76e617f1a1ee700b88fe30
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2025-05-30 22:38:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Strong Numbers/strong-numbers.py
+++ Difficulty: Basic/Strong Numbers/strong-numbers.py
@@ -1,18 +1,18 @@
 #User function Template for python3
 
 class Solution:
-    def isStrong(self, N):
-        # code here 
-        def factorial(num):
+	def is_StrongNumber(self, n):
+		# Code here
+		def factorial(num):
             result = 1
             for i in range(2,num+1):
                 result *= i
             return result
         
-        original = N
+        original = n
         sum_of_digit = 0
-        while N > 0:
-            digit = N % 10
+        while n > 0:
+            digit = n % 10
             sum_of_digit += factorial(digit)
-            N //= 10
+            n //= 10
         return 1 if original == sum_of_digit else 0
