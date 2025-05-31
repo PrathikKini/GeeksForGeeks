69d0eebf3b8953e1aa5f6f909a191196be62a741
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2025-06-01 00:59:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Decrement List Values/decrement-list-values.py
@@ -0,0 +1,7 @@
+#User function Template for python3
+
+def decrementList(arr):
+    #Write the code below to decrement the numbers by 1
+    ##After decrementing return the list
+    #Don't print it here
+    return [i - 1 for i in arr]
