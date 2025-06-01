2e2142417db503627aaaa1c17096b76fabdd5c0a
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2025-06-01 14:28:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Tuple 1/tuple-1.py
@@ -0,0 +1,9 @@
+#User function Template for python3
+
+def doubleTup(numbers):
+    
+    #Write your code to create a tuple that 
+    #holds 2*number 
+    return tuple(x*2 for x in numbers)
+        
+    #Finally return the tuple
