8a8f40fa48b5d849ebe43f0c3935aae3639e3889
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2025-06-09 16:11:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Greatest of Three/greatest-of-three.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+
+
+def find_greatest_number(a, b, c):
+    # Write your code here
+    if a>=b and a>=c:
+        return a
+    elif b>=a and b>=c:
+        return b
+    return c
