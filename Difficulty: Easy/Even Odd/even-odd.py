c88815316c0e1b2358d71fcb104f42e5ad0bf60d
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2025-06-09 14:26:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Even Odd/even-odd.py
@@ -0,0 +1,9 @@
+class Solution:
+    def evenOdd(self, a, b):
+        #Code here
+        if a %2 == 0:
+            print(a)
+            print(b)
+        else:
+            print(b)
+            print(a)
