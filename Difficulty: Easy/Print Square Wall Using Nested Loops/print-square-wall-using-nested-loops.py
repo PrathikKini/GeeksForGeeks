d57ddb28229ee3f0df650165e9ce4777c42b0107
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2025-06-09 23:12:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print Square Wall Using Nested Loops/print-square-wall-using-nested-loops.py
@@ -0,0 +1,7 @@
+#User function Template for python3
+def squareWall(s):
+    #Complete the codes below 
+    for i in range(s):
+        for j in range(s):
+            print('*',end= " ")
+        print()
