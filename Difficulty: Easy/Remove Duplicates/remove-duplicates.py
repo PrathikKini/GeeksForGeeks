246887a4f3666ac165119818635f38dca3f6bf59
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2025-06-12 23:25:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates/remove-duplicates.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+class Solution:
+	def removeDups(self, str):
+		# code here
+		l = []
+		for i in s:
+		    if i not in l:
+		        l.append(i)
+		        
+		return "".join(l)
