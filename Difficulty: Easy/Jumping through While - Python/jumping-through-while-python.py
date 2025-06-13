be1efb689bcefed4a8db8618e151dd0d7b7cf959
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2025-06-13 23:18:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Jumping through While - Python/jumping-through-while-python.py
@@ -0,0 +1,12 @@
+# Function to print x in increasing order
+def printIncreasingPower(x):
+    ##Your code here
+    i = 1 
+    # Loop to jump in powers of 2
+    while (i**2 <= x):
+        ##Your code here
+        
+        print (i**2 , end = " ")
+        i+=1
+        
+        ##Your code here
