fa13ce2e67d2da86ca6722600fa95a11438f3356
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2025-06-16 23:46:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print Square Wall 2/print-square-wall-2.py
@@ -0,0 +1,12 @@
+#User function Template for python3
+
+def squareWall(s):
+    
+    #Complete the codes below 
+    #Replace .... with your own code
+    for i in range(s):
+        
+        for j in range(s):
+            print('*',end = ' ')
+        #This print below gives new line, so you don't have to.
+        print()
