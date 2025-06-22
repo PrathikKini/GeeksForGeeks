805e8156e1bf254aa231796de2c860e6264ca078
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2025-06-22 23:37:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.py
+++ Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.py
@@ -1,12 +1,10 @@
 #User function Template for python3
-
-def fizzBuzz(number):
-    # Write your code here.
-    if number %3 == 0 and number %5 == 0:
-        print("FizzBuzz")
-    elif number % 3 == 0:
-        print('Fizz')
-    elif number % 5 == 0:
-        print('Buzz')
-    else:
-        print(number)
+a = int(input())
+if a % 3 == 0 and a % 5 == 0:
+    print("FizzBuzz")
+elif a % 3 == 0:
+    print("Fizz")
+elif a % 5 == 0:
+    print("Buzz")
+else:
+    print(a)
