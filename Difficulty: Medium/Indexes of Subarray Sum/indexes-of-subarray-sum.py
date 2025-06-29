717500c9e4781ee20295afd5fd48cdd41dcb1647
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2025-06-29 22:59:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.py
@@ -0,0 +1,18 @@
+#User function Template for python3
+class Solution:
+    def subarraySum(self, arr, target):
+        # code here
+        curr_sum = 0
+        start = 0
+        
+        for end in range(len(arr)):
+            curr_sum += arr[end]
+            
+            while curr_sum > target and start < end:
+                curr_sum -= arr[start]
+                start += 1
+            
+            if curr_sum == target:
+                return [start+1,end+1]
+        
+        return [-1]
