6dc77c05ae6a24a7c4229584f3d8645093f2044c
Prathik Kini M <64038029+PrathikKini@users.noreply.github.com>
2025-07-14 22:14:16 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: School/Swap The Numbers/swap-the-numbers.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+
+########### Write your code below ###############
+
+# Write Code to Swap
+a, b = b, a
+print(a,b)
+########### Write your code above ###############
+
